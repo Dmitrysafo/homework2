00cf6c72c8672b0ba65f208e405ad6c4067a64f0
Dmitry <Safonod@yandex.ru>
2022-06-10 00:24:11 +0300

    день недели пытаемся улучшить

---
+++ minmax/dayweek/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введи цифру, обозначающую день недели: ");
+int dayNumber = int.Parse(Console.ReadLine());
+
+
+
+    if (dayNumber == 6 || dayNumber == 7)
+    {
+      
+        Console.WriteLine("этот день выходной");
+    }
